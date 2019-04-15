37f84c91802d0299afa81a8f2183c1b83ed5d9f3
Justin Lee <julee@redhat.com>
2019-04-15 11:05:39 -0400

    update script for new lambda code

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,9 @@ function bundle() {
 
     mvn clean package -DskipTests=true -Dnative=true -Dnative-image.docker-build=true
 
-    mkdir -p ${BUNDLEDIR} target
-
-    cp -r target/bootstrap ${BUNDLEDIR}
+    mkdir -p ${BUNDLEDIR}
+    cp -r target/wiring-classes/bootstrap ${APPDIR}/*-runner* ${APPDIR}/lib ${BUNDLEDIR}
     chmod 755 ${BUNDLEDIR}/bootstrap
-    cp -r ${APPDIR}/*-runner* ${APPDIR}/lib ${BUNDLEDIR}
     cd ${BUNDLEDIR} && zip -q function.zip bootstrap function.sh *-runner* lib/* ; cd -
 
     if [[ 2 == 1 ]]
@@ -24,17 +22,17 @@ function bundle() {
     fi
 }
 
-bundle
+[ -z "$NOBUNDLE" ] && bundle
 
 clearStreams
 
 echo Deleting old function
 aws lambda delete-function \
-    --function-name bash-runtime
+    --function-name quarkus-poc
 
 echo Creating function
 aws lambda create-function \
-    --function-name bash-runtime \
+    --function-name quarkus-poc \
     --timeout 10 \
     --zip-file fileb://${BUNDLEDIR}/function.zip \
     --handler function.sh \
@@ -42,20 +40,18 @@ aws lambda create-function \
     --role ${LAMBDA_ROLE_ARN}
 
 echo
-aws lambda invoke --function-name bash-runtime --payload '{"firstName":"James", "lastName": "Lipton"}' response.txt
+time aws lambda invoke --function-name quarkus-poc --payload '{"firstName":"James", "lastName": "Lipton"}' response.txt
 cat response.txt
 echo
 
 echo
-aws lambda invoke --function-name bash-runtime --payload '{"firstName":"James", "lastName": "Halpert"}' response.txt
+time aws lambda invoke --function-name quarkus-poc --payload '{"firstName":"James", "lastName": "Halpert"}' response.txt
 cat response.txt
 echo
 
 echo
-aws lambda invoke --function-name bash-runtime --payload '{"firstName":"James", "lastName": "Jamm"}' response.txt
+time aws lambda invoke --function-name quarkus-poc --payload '{"firstName":"James", "lastName": "Jamm"}' response.txt
 cat response.txt
 echo
 
 dump
-
-cat target/*.log
